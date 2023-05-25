dbcbdab834a9c57eddc160a6628c6af4a7c86732
Benson Kamau <benkabenson@gmail.com>
2023-05-25 08:24:33 +0000

    task done

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,11 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks whether the password is valid
+    """
+    userBytes = password.encode('utf-8')
+    result = bcrypt.checkpw(userBytes, hashed_password)
+    return result
