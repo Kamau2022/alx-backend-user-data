81ede83bb30f8483a6b25c6a561fdb8c7b6c6b3d
Benson Kamau <benkabenson@gmail.com>
2023-05-26 10:08:47 +0000

    fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,9 +33,8 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         NotImplementedError
 
-    def get_db() - > mysql.connector.connection.MySQLConnection:
-        """this function returns a connector to the database
-        """
+    """def get_db() - > mysql.connector.connection.MySQLConnection:
+        this function returns a connector to the database
         host = os.getenv('PERSONAL_DATA_DB_HOST', "localhost"),
         user = os.getenv('PERSONAL_DATA_DB_USERNAME', "root"),
         password = os.getenv('PERSONAL_DATA_DB_PASSWORD', ""),
@@ -47,3 +46,20 @@ class RedactingFormatter(logging.Formatter):
                database=database
                )
         return mydb
+    """
+
+    def get_db() -> mysql.connector.connection.MySQLConnection:
+        """Creates a connector to a database.
+        """
+        db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+        db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+        db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+        db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+        connection = mysql.connector.connect(
+                     host=db_host,
+                     port=3306,
+                     user=db_user,
+                     password=db_pwd,
+                     database=db_name,
+        )
+        return connection
