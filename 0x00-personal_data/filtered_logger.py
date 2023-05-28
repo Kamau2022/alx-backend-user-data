ab8bb993da6ca0ff7e3e6db08bc45d94993cf206
Benson Kamau <benkabenson@gmail.com>
2023-05-28 16:45:06 +0000

    to  be fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,6 +20,30 @@ def filter_datum(fields: List[str], redaction: str,
     return re.sub(extract(fields, separator), replace(redaction), message)
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord):
+        """a function to filter values in incoming log records
+           using filter_datum
+        """
+        message = record.getMessage()
+        message = filter_datum(self.fields, self.REDACTION,
+                               message, self.SEPARATOR)
+        logging.basicConfig(level=logging.INFO, format=self.FORMAT)
+        logger = logging.getLogger(record.name)
+        logger.info(message)
+
+
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """a function that connects to a database.
     """
