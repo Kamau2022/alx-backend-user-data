530d59ace99e57704eeedac9f63e559c3b69260f
Benson Kamau <benkabenson@gmail.com>
2023-05-30 14:55:04 +0000

    task done

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 from api.v1.auth.auth import Auth
-
+from api.v1.auth.basic_auth import BasicAuth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -17,6 +17,8 @@ auth = None
 auth_type = os.getenv("AUTH_TYPE", 'auth')
 if auth_type == 'auth':
     auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""basic authorization
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """a class BasicAuth that inherits from Auth
+    """
+    pass
