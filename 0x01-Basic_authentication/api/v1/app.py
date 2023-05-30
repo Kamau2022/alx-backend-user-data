7b7db14371f059f04bac07748dad201a4b07ceda
Benson Kamau <benkabenson@gmail.com>
2023-05-30 13:17:03 +0000

    task done

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,39 +18,20 @@ auth_type = os.getenv("AUTH_TYPE", 'auth')
 if auth_type == 'auth':
     auth = Auth()
 
-    """@app.before_request
-    def before_request():
-     before request
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path not in excluded_paths:
-        return
-    if auth is None:
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    """
-
 
 @app.before_request
 def authenticate_user():
     """Authenticates a user before processing a request.
     """
-    if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-        ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
-            if user is None:
-                abort(403)
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        return
+    if auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
