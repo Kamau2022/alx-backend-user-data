0d3718eed49e8df569e960503a16ef6fdefa72dd
Benson Kamau <benkabenson@gmail.com>
2023-05-31 09:00:25 +0000

    task done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """basic authorization
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,22 @@ class BasicAuth(Auth):
                 return authorization_header.split()[1]
             else:
                 return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """returns the decoded value of a Base64 string
+           base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            if isinstance(base64_authorization_header, str):
+                base64_bytes = base64_authorization_header.encode('utf-8')
+                message_bytes = base64.b64decode(base64_bytes)
+                message = message_bytes.decode('utf-8')
+                return message
+        except Exception:
+            return None
