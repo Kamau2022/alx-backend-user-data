0ad258e34baab91b332d00a208a255b0bac61c6f
Benson Kamau <benkabenson@gmail.com>
2023-05-31 10:11:32 +0000

    list converted to a tuple

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,3 +43,18 @@ class BasicAuth(Auth):
                 return message
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if isinstance(decoded_base64_authorization_header, str):
+            k = ':'
+            if k not in decoded_base64_authorization_header:
+                return (None, None)
+            else:
+                return (tuple(decoded_base64_authorization_header.split(':')))
