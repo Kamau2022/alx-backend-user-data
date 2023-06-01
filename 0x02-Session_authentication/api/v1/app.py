8ea2181f4334eef91f951f20716a1d928bd82d87
Benson Kamau <benkabenson@gmail.com>
2023-06-01 14:25:33 +0000

    to be fixed

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,7 +29,9 @@ def authenticate_user():
     """Authenticates a user before processing a request.
     """
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     request.current_user = auth.current_user(request)
     if auth is None:
         return
@@ -38,6 +40,10 @@ def authenticate_user():
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.session_cookie(request) is None:
+            abort(401)
 
 
 @app.errorhandler(404)
