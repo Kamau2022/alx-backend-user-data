8ac8d857d12130ccc28343f05d6cda85b1e2f77a
Benson Kamau <benkabenson@gmail.com>
2023-06-06 17:32:39 +0000

    imported bcrypt module

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""encrypting password"""
+import bcrypt
+
+
+def _hash_password(password) -> bytes:
+    """takes in a password string arguments and returns bytes
+    """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
