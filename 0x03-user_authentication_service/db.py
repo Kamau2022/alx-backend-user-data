69a9cec6317a751a1bf2d6eb067a95028791adaf
Benson Kamau <benkabenson@gmail.com>
2023-06-07 07:15:02 +0000

    exception was added to method find_user_by

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -37,3 +38,14 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """a method to filter by input arguments
+        """
+        try:
+            query = self._session.query(User).filter_by(**kwargs)
+            return query.one()
+        except NoResultFound:
+            raise
+        except Exception as e:
+            raise InvalidRequestError(str(e))
